e0678a2bf1a91a3f7c960c05f2691cfa624a4cc6
Justbaby520 <hyc001005@gmail.com>
2024-12-23 23:40:57 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 import { connect } from "cloudflare:sockets";
 
 let password = 'baby';
-let proxyIP = 'proxyip.fxxk.dedyn.io';
+let proxyIP = 'ProxyIP.DE.tp2024.fxxk.dedyn.io';
 let sub = 'trojan.fxxk.dedyn.io'; 
 let subConverter = 'SUBAPI.fxxk.dedyn.io';// clash订阅转换后端，目前使用CM的订阅转换功能。自带虚假节点信息防泄露
 let subConfig = "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_Mini_MultiMode.ini"; //订阅配置文件
