83bc9dab9e0d4fe7507ec6fbf1f16c281cb46bb6
Justbaby520 <hyc001005@gmail.com>
2025-07-19 02:34:06 +0800

    Add files via upload

--- trojan.js
+++ trojan.js
@@ -2,7 +2,7 @@
 import { connect } from "cloudflare:sockets";
 
 let password = 'baby';
-let proxyIP = 'ProxyIP.DE.tp2024.CMLiussss.net';
+let proxyIP = '';
 let DNS64Server = '';
 //let sub = '';
 let subConverter = atob('U1VCQVBJLkNNTGl1c3Nzcy5uZXQ=');
--- vless.js
+++ vless.js
@@ -2,7 +2,7 @@
 import { connect } from 'cloudflare:sockets';
 
 let userID = 'ee49d3c3-ee3a-4b2c-a3b9-7b654700260b';
-let proxyIP = 'ProxyIP.DE.tp2024.CMLiussss.net';
+let proxyIP = '';
 let DNS64Server = '';
 //let sub = '';
 let subConverter = atob('U1VCQVBJLkNNTGl1c3Nzcy5uZXQ=');
